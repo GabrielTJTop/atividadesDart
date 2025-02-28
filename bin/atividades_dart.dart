4b8328ef486173298765dc8e0caeb09dd7214a4f
acksonx <contatoackson@gmail.com>
2025-02-28 17:25:53 -0300

    adicionando atividade 1

--- bin/atividades_dart.dart
+++ bin/atividades_dart.dart
@@ -1,120 +1,23 @@
+ // sistema de estoque e categorias de produtos de uma loja de eletronicos.
+
 import 'dart:io';
 
-double saldo = 0.0;
-
 void main() {
-  while (true) {
-    print("\nInforme uma opção (deposito, transferencia, retirada, pagamento, sair):");
-    String? operacao = stdin.readLineSync()?.toLowerCase();
+  
+  
+  List<String> categoriasValidas = ["desktop", "notebook", "smartphone", "tablet"];
+  String? categoria; 
 
-    if (operacao == "sair") {
-      print("Finalizando...");
-      break;
-    } else if (operacao == "deposito") {
-      deposito();
-    } else if (operacao == "transferencia") {
-      transferencia();
-    } else if (operacao == "retirada") {
-      retirada();
-    } else if (operacao == "pagamento") {
-      pagamento();
+  do {
+   
+    print("Digite a categoria do produto (desktop, notebook. smartphone, tablet):");
+    categoria = stdin.readLineSync(); 
+
+    
+    if (categoriasValidas.contains(categoria)) {
+      print("Categoria válida: $categoria");
     } else {
-      print("Opção inválida. Tente novamente.");
+      print("Categoria inválida");
     }
-  }
-}
-
-void deposito() {
-  double valor = 0.0;
-  while (valor <= 0) {
-    print("Informe o valor para depósito:");
-    String? entrada = stdin.readLineSync();
-    
-    valor = double.tryParse(entrada ?? '') ?? 0.0;
-
-    if (valor <= 0) {
-      print("Valor inválido. Tente novamente.");
-    }
-  }
-
-  saldo += valor;
-  print("Depósito de R\$${valor.toStringAsFixed(2)} realizado com sucesso.");
-  print("Saldo atual: R\$${saldo.toStringAsFixed(2)}");
-}
-
-void transferencia() {
-  if (saldo <= 0) {
-    print("Saldo insuficiente para realizar transferência.");
-    return;
-  }
-
-  double valor = 0.0;
-  while (valor <= 0 || valor > saldo) {
-    print("Informe o valor para transferência:");
-    String? entrada = stdin.readLineSync();
-    
-    valor = double.tryParse(entrada ?? '') ?? 0.0;
-
-    if (valor <= 0) {
-      print("Valor inválido. Tente novamente.");
-    } else if (valor > saldo) {
-      print("Saldo insuficiente. Tente um valor menor.");
-    }
-  }
-
-  print("Informe o número da conta do destinatário:");
-  String? contaDestino = stdin.readLineSync();
-
-  if (contaDestino != null && contaDestino.isNotEmpty) {
-    saldo -= valor;
-    print("Transferência de R\$${valor.toStringAsFixed(2)} para a conta $contaDestino realizada com sucesso.");
-    print("Saldo atual: R\$${saldo.toStringAsFixed(2)}");
-  } else {
-    print("Conta de destino inválida.");
-  }
-}
-
-void retirada() {
-  if (saldo <= 0) {
-    print("Saldo insuficiente para realizar retirada.");
-    return;
-  }
-
-  double valor = 0.0;
-  while (valor <= 0 || valor > saldo) {
-    print("Informe o valor para retirada:");
-    String? entrada = stdin.readLineSync();
-    
-    valor = double.tryParse(entrada ?? '') ?? 0.0;
-
-    if (valor <= 0) {
-      print("Valor inválido. Tente novamente.");
-    } else if (valor > saldo) {
-      print("Saldo insuficiente. Tente um valor menor.");
-    }
-  }
-
-  saldo -= valor;
-  print("Retirada de R\$${valor.toStringAsFixed(2)} realizada com sucesso.");
-  print("Saldo atual: R\$${saldo.toStringAsFixed(2)}");
-}
-
-void pagamento() {
-  double valor = 0.0;
-  while (valor <= 0 || valor > saldo) {
-    print("Informe o valor para pagamento:");
-    String? entrada = stdin.readLineSync();
-    
-    valor = double.tryParse(entrada ?? '') ?? 0.0;
-
-    if (valor <= 0) {
-      print("Valor inválido. Tente novamente.");
-    } else if (valor > saldo) {
-      print("Saldo insuficiente. Tente um valor menor.");
-    }
-  }
-
-  saldo -= valor;
-  print("Pagamento de R\$${valor.toStringAsFixed(2)} realizado com sucesso.");
-  print("Saldo atual: R\$${saldo.toStringAsFixed(2)}");
+  } while (!categoriasValidas.contains(categoria));
 }
